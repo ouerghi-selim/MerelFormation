12dc8ed73bc0bea14ba578c6c53b0471df8b189b
SELIM OUERGHI <ouerghi.selim@gmail.com>
2025-01-29 23:59:44 +0100

    fix: update vite config and index.html for correct asset handling

--- app/assets/vite.config.ts
+++ app/assets/vite.config.ts
@@ -2,41 +2,37 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-
 export default defineConfig({
-  root: './', // Définit le dossier racine pour Vite
+  root: './app/assets',
   base: '/',
   build: {
-    //outDir: './public/build',
-    outDir: 'dist',
+    outDir: '../public/build',
     assetsDir: 'assets',
     emptyOutDir: true,
-    sourcemap: true,
     manifest: true,
     rollupOptions: {
-      // input: './src/main.tsx',
-      // output: {
-      //   entryFileNames: 'assets/[name]-[hash].js',
-      //   chunkFileNames: 'assets/[name]-[hash].js',
-      //   assetFileNames: 'assets/[name]-[hash][extname]',
-      input: path.resolve(__dirname, './index.html'),
-      // },
-    },
+      input: path.resolve(__dirname, 'index.html'),
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Permet d'utiliser des imports absolus
-      '@assets': path.resolve(__dirname, './src/assets')
-    },
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
+    }
   },
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permet au serveur d’écouter sur toutes les interfaces réseau
-    port: 5173, // Définit le port
-    strictPort: true, // Fait échouer le démarrage si le port est occupé
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     hmr: {
-      host: 'localhost', // Utilisé pour le Hot Module Replacement
-    },
+      host: 'localhost'
+    }
   },
   preview: {
     port: 4173,
